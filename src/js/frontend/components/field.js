4bd1c376f785a00fe50ef40fefdedfdf028070a8
Mikhail Sementsov <fewebdev@gmail.com>
2019-01-16 19:00:04 +0300

    initial

--- src/js/frontend/components/field.js
+++ src/js/frontend/components/field.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { pre } from "./pre";
 
+/*
 const mark = `
 
-div(id="d00")
+div(id="d00") 7
 div(id="d0")
   div(id="d1")
     div(id="d2")
@@ -11,6 +12,21 @@ div(id="d0")
       div(id="d3")
     div(id="d4")
       div(id="d5")
+        input(className="input input-main" type="tel" autoFocus required)
+
+`;
+*/
+
+const mark = `
+
+div(id="d00") 1
+div(id="d0") 2
+  div(id="d1") 3
+    div(id="d2") 4
+    div(id="d21") 5
+      div(id="d3") 6
+    div(id="d4") 7
+     
 
 `;
 
--- src/js/frontend/components/pre.js
+++ src/js/frontend/components/pre.js
@@ -4,7 +4,119 @@ const log = console.log.bind(console);
 const crel = React.createElement;
 const frag = React.Fragment;
 
-function slicer(str) {
+function setAttributes(str) {
+	const attrStr = str.slice(str.indexOf("(") + 1, str.indexOf(") "));
+	const attrs = attrStr.match(/\w*=".*?"/g) || [];
+	attrStr.split(/\w*=".*?"/).map((item) => {
+		const items = item.trim().split(" ");
+		items.map((sn) => {
+			if (sn) attrs.push(`${sn}=""`);
+		});
+	});
+
+	const obj = {};
+	attrs.map((item) => {
+		let [attr, val] = item.split("=");
+		val = val.replace(/"/g, "");
+		Object.defineProperty(obj, attr, {
+			value: val || true,
+			enumerable: true,
+		});
+	});
+
+	obj.key = "key" + Math.round(1e15 * Math.random());
+	return obj;
+}
+
+//log(setAttributes(`input(class="input input-main" type="tel" autoFocus required)`));
+
+function findValues(str) {
+	log(str);
+	let vals = str.match(/#\[.*?\]/g);
+	let res = [];
+
+	if (!str) return { first: "", tail: [] };
+	if (!vals) return { first: str, tail: [] };
+
+	vals.reduce((acc, cur, i) => {
+		const temp = acc.split(cur);
+		cur = cur.slice(2, -1);
+
+		res.push(temp[0], cur);
+		if (i === vals.length - 1) res.push(temp[1]);
+		return temp[1];
+	}, str);
+
+	const [first, ...tail] = res.filter((item) => item);
+	return { first, tail };
+}
+
+let mark = `
+
+div(class="input input-main" id="d5") 15
+
+`;
+
+//log(findValues(mark));
+
+const createSpaces = (num = 0, tabSize = 2) =>
+	[...Array(num)].reduce((acc) => acc + "	", "");
+
+function findValues2(str) {
+	let vals = str.match(/#\[.*?\]/g);
+	let res = [];
+
+	vals.reduce((acc, cur, i) => {
+		const temp = acc.split(cur);
+		cur = cur.slice(2, -1);
+
+		res.push(temp[0], cur);
+		if (i === vals.length - 1) res.push(temp[1]);
+		return temp[1];
+	}, str);
+
+	const [first, ...preTail] = res.filter((item) => item);
+	return { first, preTail };
+}
+
+const test = [
+	`div(class="input input-main" type="tel" required) 10`,
+	`div(class="input input-main" type="tel" required)`,
+	`5`,
+	`div(class="input input-main" type="tel" required) 1#[span 2]3#[span 4]5`,
+];
+
+function splitRow(str, tab, tail) {
+	const case03 = str.match(/^\w*\(.*?\) .*/) || "",
+		case1 = str.match(/^\w*\(.*?\)$/) || "",
+		case3 = str.match(/#\[.*?\]/) || "";
+
+	let tag = str.slice(0, str.indexOf("(")),
+		attr = str.slice(str.indexOf("(") + 1, str.indexOf(")")),
+		val = str.slice(str.indexOf(")") + 2),
+		preTail = [""];
+
+	if (case03 && case3) {
+		const values = findValues2(val);
+		[val, preTail] = [values.first, values.preTail];
+	} else if (case03 && !case3);
+	else if (case1) val = null;
+	else [tag, attr, val] = [null, null, str];
+
+	if (attr) attr = setAttributes(str);
+
+	const shift = createSpaces(tab + 1);
+	preTail = preTail.reduce((acc, cur) => acc + shift + cur + "\n", "");
+	if (preTail.trim()) tail = preTail + tail;
+
+	return { tag, attr, val, tail };
+}
+
+//test.map((item) => splitRow(item, 1, ""));
+
+function slicer(str, tabSize = 2) {
+	//	const val = str.match(/^\s*?\w*\(.*?\) .*/g);
+	// get first row
 	const ind = str.indexOf("\n");
 	let first, tail;
 	if (ind === -1) [first, tail] = [str, null];
@@ -12,13 +124,20 @@ function slicer(str) {
 	let tab = first.match(/ {2}/g);
 	tab = tab === null ? 0 : tab.length;
 	first = first.trim();
-	let [tag, attrs] = [first.match(/\w*/)[0], first.match(/\w*="\w*"/)[0]]; // simplified
-	attrs = Object.defineProperty({}, attrs.split("=")[0], {
-		value: attrs.split("=")[1].replace(/"/g, ""),
-		enumerable: true,
-	});
-	attrs.key = "key" + Math.round(1e15 * Math.random());
-	return { tab, tag, attrs, tail };
+
+	// process first row
+	const s = splitRow(first, tab, tail);
+	const { tag, val } = s;
+	tail = s.tail;
+	const attrs = s.attr;
+
+	// let [tag, attrs, val] = [
+	// 	first.match(/\w*/)[0],
+	// 	setAttributes(first),
+	// 	first.slice(first.indexOf(") ") + 2),
+	// ];
+
+	return { tab, tag, attrs, val, tail };
 }
 
 function ext(mark) {
@@ -26,16 +145,17 @@ function ext(mark) {
 	mark = mark.trim();
 
 	function thr(str) {
-		const { tab, tag, attrs, tail } = slicer(str);
+		const { tab, tag, attrs, val, tail } = slicer(str);
+		log({ tab, tag, attrs, val, tail });
 
 		if (tail) {
 			thr(tail);
-			if (tabPre > tab) res = [crel(tag, attrs, ...res)];
-			else if (tabPre === tab) res = [crel(tag, attrs), ...res];
+			if (tabPre > tab) res = [crel(tag, attrs, val, ...res)];
+			else if (tabPre === tab) res = [crel(tag, attrs, val), ...res];
 			else {
 				depth = tabPre;
 				store = res;
-				res = tab === depth ? [] : [crel(tag, attrs)];
+				res = tab === depth ? [] : [crel(tag, attrs, val)];
 			}
 			if (tab === depth) {
 				res = [...res, ...store];
@@ -44,7 +164,7 @@ function ext(mark) {
 			tabPre = tab;
 			return crel(frag, null, ...res);
 		} else {
-			res = [crel(tag, attrs)];
+			res = [crel(tag, attrs, val)];
 			tabPre = tab;
 		}
 	}
