97253616e0004e53a9a686cd27fc13b13fff4d6c
Mikhail Sementsov <fewebdev@gmail.com>
2019-01-20 22:15:17 +0300

    0.5.0

--- src/js/frontend/components/field.js
+++ src/js/frontend/components/field.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import pre from "pug-to-react-element";
-//import pre from "./pre";
+//import pre from "pug-to-react-element";
+import pre from "./pre";
 //pre.tabSize = 4;
 
 const markup = `
@@ -18,7 +18,19 @@ div(onClick={handler}) pow: {cnt} W {cnt2} gyuj
       div(id="d4") 7
     input(className="input input-main" type="tel" autoFocus required)
        
-  `;
+`;
+
+// const markup = `
+
+// div(id="d0")
+//   div(id="d1")
+//     div(id="d2")
+//     div(id="d21")
+//       div(id="d3")
+//     div(id="d4")
+//       div(id="d5" onClick={handler}) cnt = {cnt} y
+
+// `;
 
 class Field extends Component {
 	state = { cnt: 0, cnt2: 0 };
