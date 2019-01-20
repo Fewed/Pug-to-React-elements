5d6ccc9d6d7c0f2d409234e0a425aa664fed1184
Mikhail Sementsov <fewebdev@gmail.com>
2019-01-20 22:22:43 +0300

    0.5.0

--- src/js/frontend/components/field.js
+++ src/js/frontend/components/field.js
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
-//import pre from "pug-to-react-element";
-import pre from "./pre";
+import pre from "pug-to-react-element";
+//import pre from "./pre";
 //pre.tabSize = 4;
-
-const markup = `
-
-div(id="d00") 0#[span() 1]2#[span() 3]4
-div(id="d0")
-  div(id="d1") 3
-    31
-    div(id="d2") 4
-div(onClick={handler}) pow: {cnt} W {cnt2} gyuj
-  div() u7
-    div(id="d21") m9
-  div(id="d3") 6
-    div() 87
-      div(id="d4") 7
-    input(className="input input-main" type="tel" autoFocus required)
-       
-`;
 
 // const markup = `
 
+// div(id="d00") 0#[span() 1]2#[span() 3]4
 // div(id="d0")
-//   div(id="d1")
-//     div(id="d2")
-//     div(id="d21")
-//       div(id="d3")
-//     div(id="d4")
-//       div(id="d5" onClick={handler}) cnt = {cnt} y
+//   div(id="d1") 3
+//     31
+//     div(id="d2") 4
+// div(onClick={handler}) pow: {cnt} W {cnt2} gyuj
+//   div() u7
+//     div(id="d21") m9
+//   div(id="d3") 6
+//     div() 87
+//       div(id="d4") 7
+//     input(className="input input-main" type="tel" autoFocus required)
 
 // `;
+
+const markup = `
+
+div(id="d0")
+  div(id="d1")
+    div(id="d2")
+    div(id="d21")
+      div(id="d3")
+    div(id="d4")
+      div(id="d5" onClick={handler}) cnt = {cnt} y
+
+`;
 
 class Field extends Component {
 	state = { cnt: 0, cnt2: 0 };
